28b1393cb45ab90488c4b86792ab112084075967
Hannan <najma.mahat@student.moringaschool.com>
2023-05-08 20:11:49 +0300

    Completed controller validations

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,5 @@
 class AuthorsController < ApplicationController
-  
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_params
   def show
     author = Author.find(params[:id])
 
@@ -7,15 +7,20 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
 
   private
-  
+
   def author_params
     params.permit(:email, :name)
   end
-  
+
+  def invalid_params(invalid)
+    render json: {
+             errors: invalid.record.errors
+           },
+           status: :unprocessable_entity
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,16 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_post_params
+  rescue_from ActiveRecord::RecordNotFound, with: :post_not_found
 
   def show
     post = Post.find(params[:id])
-    
+
     render json: post
   end
 
   def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post = find_post
+    post.update!(post_params)
     render json: post
   end
 
@@ -20,4 +20,18 @@ class PostsController < ApplicationController
     params.permit(:category, :content, :title)
   end
 
+  def find_post
+    Post.find(params[:id])
+  end
+
+  def invalid_post_params(invalid)
+    render json: {
+             errors: invalid.record.errors
+           },
+           status: :unprocessable_entity
+  end
+
+  def post_not_found
+    render json: { error: "Post not found" }, status: :not_found
+  end
 end
